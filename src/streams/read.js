0bc3f398251693d7c986bf40966050fe824b13d9
Grigorii Iavorskii <yavgan06@gmail.com>
2022-06-05 16:47:59 +0400

    feat: streams task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fd = await fs.open(path.resolve(__dirname,'files','fileToRead.txt'));
+	const inputStream = fd.createReadStream({ encoding: 'utf-8' });
+	inputStream.pipe(process.stdout);
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const write = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const fd = await fs.open(path.resolve(__dirname,'files','fileToWrite.txt'), 'w');
+	const outputStream = fd.createWriteStream({ encoding: 'utf-8' });
+	process.stdin.pipe(outputStream);
 };
+
+write();
