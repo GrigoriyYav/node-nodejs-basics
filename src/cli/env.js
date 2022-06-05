3eb452f5b051d9cb7121c62c4a5b391ff293b026
Grigorii Iavorskii <yavgan06@gmail.com>
2022-06-05 14:31:34 +0400

    feat: cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,20 @@
 export const parseEnv = () => {
-    // Write your code here 
+	const outputData = [];
+
+	for (let key in process.env) {
+		if (key.startsWith('RSS_')) {
+			outputData.push(key + '=' + process.env[key]);
+		}
+	}
+
+	console.log(outputData.join('; '));
 };
+
+// for test
+
+//for Windows PowerShell:
+//$env:RSS_1="Hello"; $env:RSS_2="World"; npm run cli:env
+
+//# for bash
+//RSS_1=Hello RSS_2=World npm run cli:env
+parseEnv();
